05fe9f3661b0171c55ea8bfee93568f9d5938e26
Kashish Dhawan <56489521+Kashishdwn@users.noreply.github.com>
2021-11-26 14:47:44 +0530

    Add files via upload

---
+++ ami_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+rhel8_raw="ami-06a0b4e3b7eb7a300"
+region="ap-south-1"
+aminame="rhel8_kashish"
+security_group_id="sg-05f534737fdfc9f7c"
+
+cat <<EOF > var.json
+      {
+        "rhel8_raw": "${rhel8_raw}",
+        "security_group_id": "${security_group_id}",
+        "aminame": "${aminame}",
+        "region": "${region}"
+      }
+EOF
+${PACKER_BINARY} build -var-file var.json templates/rhel8.json
